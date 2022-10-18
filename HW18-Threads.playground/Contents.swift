174eabc43c44db76d320108a7e37f4d7276550a8
Nikolay Spravtsev <nicksprv@icloud.com>
2022-10-18 22:57:01 +0300

    Generating thread class added

--- HW18-Threads.playground/Contents.swift
+++ HW18-Threads.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class GeneratingThread: Thread {
+    let timePeriod = 2
+    let generatingPeriod = 20
+
+    override func main() {
+        var limiter = 0
+
+        Timer.scheduledTimer(withTimeInterval: TimeInterval(timePeriod), repeats: true) { timer in
+            let chip = Chip.make()
+            // add chip to the storage
+            limiter += Int(timer.timeInterval)
+
+            if limiter >= self.generatingPeriod {
+                timer.invalidate()
+            }
+        }
+    }
+}
+
+class WorkThread: Thread {
+
+}
